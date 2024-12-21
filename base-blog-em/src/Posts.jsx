a958bbf619988d1f2a7c3fbb5305706d4217b074
seok-hyung <korea970223@naver.com>
2024-12-22 00:12:56 +0900

    ✨ 다음 페이지 데이터 prefetching 하여 사용자 경험 개선선

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { fetchPosts, deletePost, updatePost } from './api'
 import { PostDetail } from './PostDetail'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 const maxPostPage = 10
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1)
   const [selectedPost, setSelectedPost] = useState(null)
+
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1
+      queryClient.prefetchQuery({
+        queryKey: ['posts', nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      })
+    }
+    // 이 prefetchQuery에서 사용되는 쿼리키는 useQuery에서 사용된 것과 타입이 같아야 한다.
+    // 왜냐하면 리액트쿼리가 캐시에 이미 데이터가 있는지 확인할때 여기를 확인해야하기 때문이다.
+  }, [currentPage, queryClient])
+  // 이걸 언제 실행해야할까??
+  // 다음 페이지 버튼의 클릭 이벤트에서 이걸 실행하는 것은 좋지 않다.
+  // 왜냐하면 상태 업데이트는 비동기적이라 이 업데이트가 이미 적용되었는지 정확히 알 수 없기 때문이다.
+  // 현재 페이지가 무엇인지 확실히 알 수 있는 알맞은 방법이 없다.
+  // 그래서, 현재 페이지가 변경될떄마다 useEffect를 사용할 것이다.
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['posts', currentPage],
