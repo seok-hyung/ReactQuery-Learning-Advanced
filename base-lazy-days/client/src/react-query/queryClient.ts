cc956cb7a39c165d8eba5783c89cd1c36a401a0c
seok-hyung <korea970223@naver.com>
2025-01-13 23:43:23 +0900

    ✨ mutaion 에러핸들러 전역에서 관리

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,18 +1,21 @@
-import { QueryCache, QueryClient } from '@tanstack/react-query'
-import { toast } from '@/components/app/toast'
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
+import { toast } from "@/components/app/toast";
 
-function errorHandler(errorMsg: string) {
+function createTitle(errorMsg: string, actionType: "query" | "mutation") {
+  const action = actionType === "query" ? "fetch" : "update";
+  return `could not ${action} data: ${
+    errorMsg ?? "error connecting to server"
+  }`;
+}
+
+function errorHandler(title: string) {
   // one message per page load, not one message per query
   // the user doesn't care that there were three failed queries on the staff page
   //    (staff, treatments, user)
-  const id = 'react-query-toast' // 중복된 토스트가 표시되지 않는지 확인할 수 있도록 토스트에 Id를 지정한다.
+  const id = "react-query-toast"; // 중복된 토스트가 표시되지 않는지 확인할 수 있도록 토스트에 Id를 지정한다.
 
   if (!toast.isActive(id)) {
-    const action = 'fetch' // useQury 에러인지, useMutation 에러인지에 따라서 이 값을 변화시켜준다.
-    const title = `could not ${action} data: ${
-      errorMsg ?? 'error connecting to server'
-    }`
-    toast({ id, title, status: 'error', variant: 'subtle', isClosable: true })
+    toast({ id, title, status: "error", variant: "subtle", isClosable: true });
   }
 }
 
@@ -25,8 +28,14 @@ export const queryClient = new QueryClient({
     },
   },
   queryCache: new QueryCache({
-    onError: error => {
-      errorHandler(error.message)
+    onError: (error) => {
+      const title = createTitle(error.message, "query");
+      errorHandler(title);
     },
   }),
-})
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      errorHandler(error.message);
+    },
+  }),
+});
