974063e19a8340e7e07f822545df3f5becc4b6f0
seok-hyung <korea970223@naver.com>
2025-01-14 23:09:44 +0900

    ✨ 예약취소, invalidateQueries

--- base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -3,20 +3,30 @@ import { Appointment } from "@shared/types";
 import { axiosInstance } from "@/axiosInstance";
 import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 import { queryKeys } from "@/react-query/constants";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 // for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: "remove", path: "/userId" }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 export function useCancelAppointment() {
+  const queryClient = useQueryClient();
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    mutationFn: removeAppointmentUser,
+    // 전달할 인수가 mutatie 함수에 전달할 것과 동일하기 때문에 인자를 받아올 필요가 없다?
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.appointments] });
+      toast({
+        title: "You have canceled the appointment!",
+        status: "warning",
+      });
+    },
+  });
+  return mutate;
 }
